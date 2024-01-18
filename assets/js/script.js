2c6e03aaeb77843868003b3cda4b27051b24dcd8
Silvia Saverino <silviasave89@gmail.com>
2024-01-18 19:25:53 +0000

    add js files and content to render posts and fix medium screen size image style

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+import {postArray} from "./posts.js"
+
+const singlePostHtml = postArray.map(function(post){
+    console.log(post)
+    return `
+    <div class="posts">
+        <img class="post-img" src="${post.image}">
+        <div class="post-info">
+            <h3>${post.title}</h3>
+            <p> ${post.preview} </p>
+        </div>
+    </div>
+`
+})
+
+document.getElementById("container").innerHTML = singlePostHtml
+
