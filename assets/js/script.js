348afd284483c5a9431ea2d319aadbde78c2c264
Silvia Saverino <silviasave89@gmail.com>
2024-01-20 21:00:39 +0000

    add functionality to get current page and if user visits 'about' page render posts only when the user clicks on the 'recent posts' button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,31 @@
 import {postArray} from "./posts.js"
+const revealPostBtn = document.getElementById("reveal-post-btn")
+var currentPage = window.location.pathname
 
-const singlePostHtml = postArray.map(function(post){
-    return `
-    <div class="posts">
-        <img class="post-img" src="${post.image}">
-        <div class="post-info">
-            <h3>${post.title}</h3>
-            <p> ${post.preview} </p>
+function renderPosts(){
+    const singlePostHtml = postArray.map(function(post){
+        return `
+        <div class="posts">
+            <img class="post-img" src="${post.image}">
+            <div class="post-info">
+                <h3>${post.title}</h3>
+                <p> ${post.preview} </p>
+            </div>
         </div>
-    </div>
-`
-}).join('')
+    `
+    }).join('')
 
-document.getElementById("container").innerHTML = singlePostHtml
+    document.getElementById("container").innerHTML = singlePostHtml
+}
+
+if (currentPage === '/about.html') {
+    document.getElementById("container").innerHTML = ''
+    console.log('this is the about page')
+} else {
+    renderPosts()
+}
+
+revealPostBtn.addEventListener("click", function() {
+    renderPosts()
+})
 
