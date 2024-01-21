6a670ebd88dfac4fd888f135c798e8a251ee186c
Silvia Saverino <silviasave89@gmail.com>
2024-01-21 18:09:21 +0000

    add functionality to render the content of the clicked post with a modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,13 +10,14 @@ function renderPosts() {
           <div class="post-info">
             <h3>${post.title}</h3>
             <p>${post.preview}</p>
+            <p class="post-content hide">${post.content}</p>
           </div>
         </div>
       `
     }).join('')
   
     document.getElementById("container").innerHTML = singlePostHtml
-    
+
     //Assign click event listeners to each post for handling post clicks
     const posts = document.querySelectorAll('.posts')
     posts.forEach(post => post.addEventListener('click', handlePostClick))
@@ -40,4 +41,31 @@ function renderPosts() {
     // Find the selected post in postArray based on the clicked id
     const selectedPost = postArray.find(post => post.id.toString() === postId)
     console.log(selectedPost)
+    // renderClickedPostContent(postId)
+    renderPostModal(selectedPost)
   }
+
+//   function renderClickedPostContent(postId){
+//     const postContents = document.querySelectorAll('.post-content');
+//     postContents.forEach(content => content.classList.add("hide"));
+
+//     const clickedPostContent = document.getElementById(`${postId}`).querySelector('.post-content');
+//     clickedPostContent.classList.remove("hide");
+// }
+
+function renderPostModal(selectedPost) {
+    // Create and show a modal
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+    modal.innerHTML = `
+        <div class="modal">
+            <button class="modal-close-btn" id="modal-close-btn" disabled>X</button>
+            <img src="${selectedPost.image}" alt="${selectedPost.title}" class="post-img ">
+            <div class=""modal-content">
+                <h3>${selectedPost.title}</h3>
+                <p>${selectedPost.content}</p>
+            </div>
+        </div>
+    `
+    document.body.appendChild(modal);
+}
